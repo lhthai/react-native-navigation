3c6965348d4ad2313f6d4d9fa7b9e083b98d588d
Le Hong Thai <thai.le@action-composites.com.vn>
2020-05-26 20:39:08 +0700

    add

--- App.js
+++ App.js
@@ -1,73 +1,19 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
-import {createDrawerNavigator} from '@react-navigation/drawer';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import HomeScreen from './components/HomeScreen';
-import DetailsScreen from './components/DetailsScreen';
+import MainTabsScreen from './components/MainTabsScreen'
+import DrawerContent from './components/DrawerContent'
 
-const Stack = createStackNavigator();
-const HomeStack = createStackNavigator();
-const DetailStack = createStackNavigator();
 const Drawer = createDrawerNavigator();
-
-const HomeStackScreen = ({navigation}) => (
-  <HomeStack.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: '#009387',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    }}>
-    <Stack.Screen
-      name="Home"
-      component={HomeScreen}
-      options={{
-        title: 'Home',
-        headerTitleAlign: 'center',
-        headerLeft: () => (
-          <Icon.Button
-            name="ios-menu"
-            size={25}
-            backgroundColor="#009387"
-            onPress={() => navigation.openDrawer()}
-          />
-        ),
-      }}
-    />
-  </HomeStack.Navigator>
-);
-
-const DetailStackScreen = ({navigation}) => (
-  <DetailStack.Navigator
-    screenOptions={{
-      headerStyle: {
-        backgroundColor: '#009387',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
-    }}>
-    <Stack.Screen
-      name="Detail"
-      component={DetailsScreen}
-      options={{title: 'Detail', headerTitleAlign: 'center'}}
-    />
-  </DetailStack.Navigator>
-);
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Home" component={HomeStackScreen} />
-        <Drawer.Screen name="Detail" component={DetailStackScreen} />
+      <Drawer.Navigator drawerContent={props => <DrawerContent {...props} />}>
+        <Drawer.Screen name="Home" component={MainTabsScreen} />
+        {/* <Drawer.Screen name="Detail" component={DetailStackScreen} /> */}
       </Drawer.Navigator>
     </NavigationContainer>
   );
